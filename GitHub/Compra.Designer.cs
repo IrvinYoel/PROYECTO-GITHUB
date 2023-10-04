32619989b44a163b5e3480d0a5ad56cfa32ef021
Irvin Yoel <irvinyoel.cerna@gmail.com>
2023-10-03 23:16:14 -0500

    Creación Form Compra

---
+++ GitHub/Compra.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace GitHub
+{
+    partial class Compra
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblRequerimiento = new System.Windows.Forms.Label();
+            this.txtRequerimiento = new System.Windows.Forms.TextBox();
+            this.lblOC = new System.Windows.Forms.Label();
+            this.txtOrdenCompra = new System.Windows.Forms.TextBox();
+            this.lblMonto = new System.Windows.Forms.Label();
+            this.txtMonto = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblRequerimiento
+            // 
+            this.lblRequerimiento.AutoSize = true;
+            this.lblRequerimiento.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRequerimiento.Location = new System.Drawing.Point(12, 21);
+            this.lblRequerimiento.Name = "lblRequerimiento";
+            this.lblRequerimiento.Size = new System.Drawing.Size(113, 20);
+            this.lblRequerimiento.TabIndex = 0;
+            this.lblRequerimiento.Text = "Requerimiento";
+            // 
+            // txtRequerimiento
+            // 
+            this.txtRequerimiento.Location = new System.Drawing.Point(183, 23);
+            this.txtRequerimiento.Name = "txtRequerimiento";
+            this.txtRequerimiento.Size = new System.Drawing.Size(160, 20);
+            this.txtRequerimiento.TabIndex = 1;
+            // 
+            // lblOC
+            // 
+            this.lblOC.AutoSize = true;
+            this.lblOC.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOC.Location = new System.Drawing.Point(12, 69);
+            this.lblOC.Name = "lblOC";
+            this.lblOC.Size = new System.Drawing.Size(135, 20);
+            this.lblOC.TabIndex = 2;
+            this.lblOC.Text = "Orden de Compra";
+            // 
+            // txtOrdenCompra
+            // 
+            this.txtOrdenCompra.Location = new System.Drawing.Point(183, 69);
+            this.txtOrdenCompra.Name = "txtOrdenCompra";
+            this.txtOrdenCompra.Size = new System.Drawing.Size(160, 20);
+            this.txtOrdenCompra.TabIndex = 3;
+            // 
+            // lblMonto
+            // 
+            this.lblMonto.AutoSize = true;
+            this.lblMonto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMonto.Location = new System.Drawing.Point(12, 109);
+            this.lblMonto.Name = "lblMonto";
+            this.lblMonto.Size = new System.Drawing.Size(54, 20);
+            this.lblMonto.TabIndex = 4;
+            this.lblMonto.Text = "Monto";
+            // 
+            // txtMonto
+            // 
+            this.txtMonto.Location = new System.Drawing.Point(183, 109);
+            this.txtMonto.Name = "txtMonto";
+            this.txtMonto.Size = new System.Drawing.Size(160, 20);
+            this.txtMonto.TabIndex = 5;
+            // 
+            // Compra
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 217);
+            this.Controls.Add(this.txtMonto);
+            this.Controls.Add(this.lblMonto);
+            this.Controls.Add(this.txtOrdenCompra);
+            this.Controls.Add(this.lblOC);
+            this.Controls.Add(this.txtRequerimiento);
+            this.Controls.Add(this.lblRequerimiento);
+            this.Name = "Compra";
+            this.Text = "Compra";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblRequerimiento;
+        private System.Windows.Forms.TextBox txtRequerimiento;
+        private System.Windows.Forms.Label lblOC;
+        private System.Windows.Forms.TextBox txtOrdenCompra;
+        private System.Windows.Forms.Label lblMonto;
+        private System.Windows.Forms.TextBox txtMonto;
+    }
+}
