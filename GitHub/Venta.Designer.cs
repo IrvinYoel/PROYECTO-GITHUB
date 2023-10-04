edb08b45f0e06d9a802d95937e50dcf7cb939dcd
Irvin Yoel <irvinyoel.cerna@gmail.com>
2023-10-04 00:42:21 -0500

    Creación botón vender

--- GitHub/Venta.Designer.cs
+++ GitHub/Venta.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.txtProductos = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -48,11 +49,21 @@
             this.txtProductos.Size = new System.Drawing.Size(100, 20);
             this.txtProductos.TabIndex = 1;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(246, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Vender";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Venta
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(468, 124);
+            this.ClientSize = new System.Drawing.Size(396, 91);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.txtProductos);
             this.Controls.Add(this.label1);
             this.Name = "Venta";
@@ -66,5 +77,6 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtProductos;
+        private System.Windows.Forms.Button button1;
     }
 }
